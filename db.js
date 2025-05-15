9a3c49f72fc16a70d6466d5ba7c132f3030b2ece
RAKSHAN-D <rakshandn@gmail.com>
2025-05-16 00:40:27 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,14 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "MAJOR_PROJECT",
-  password: "rakshan@SDM069",
-  port: 5432, // default PostgreSQL port
+  user: 'rakshan',
+  host: 'dpg-d0j3jh7diees73cvotpg-a',
+  database: 'major_project_cx19',
+  password: process.env.DB_PASSWORD,  // set this in your Render environment variables
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-export default pool;
+export default pool; 
